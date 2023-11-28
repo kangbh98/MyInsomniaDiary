b9129a290faa66f48097cee198555b0308ed5218
정우성 <>
2023-11-29 00:09:26 +0900

    로그인 기능 구현

--- backend/src/main/frontend/my-insomnia-diary/src/pages/Login.jsx
+++ backend/src/main/frontend/my-insomnia-diary/src/pages/Login.jsx
@@ -1,6 +1,60 @@
 import { Link } from 'react-router-dom';
+import {useState} from "react";
+import axios from "axios";
 
 const Login = () => {
+
+  const baseUrl = "http://localhost:8080";
+  const onClickLogin = () => {
+
+    console.log("click login");
+    console.log("ID : ", email);
+    console.log("PW : ", password);
+
+    axios
+        .post(baseUrl + "/login", {
+          email: email,
+          passwd: password,
+        })
+        .then((res) => {
+          console.log(res);
+          console.log("res.data.userId :: ", res.data.email);
+          console.log("res.data.msg :: ", res.data.password);
+          if (res.data.email === undefined) {
+            // id 일치하지 않는 경우 userId = undefined, msg = '입력하신 id 가 일치하지 않습니다.'
+            console.log("======================");
+            alert("입력하신 id 가 일치하지 않습니다.");
+
+          } else if (res.data.email === null) {
+            // id는 있지만, pw 는 다른 경우 userId = null , msg = undefined
+            console.log(
+                "입력하신 비밀번호 가 일치하지 않습니다."
+            );
+            alert("입력하신 비밀번호 가 일치하지 않습니다.");
+
+          } else if (res.data.email === email) {
+            // id, pw 모두 일치 userId = userId1, msg = undefined
+            console.log("======================", "로그인 성공");
+            sessionStorage.setItem("user_id", inputId); // sessionStorage에 id를 user_id라는 key 값으로 저장
+            sessionStorage.setItem("name", res.data.name); // sessionStorage에 id를 user_id라는 key 값으로 저장
+          }
+          // 작업 완료 되면 페이지 이동(새로고침)
+          document.location.href = "/home";
+        })
+        .catch();
+  };
+
+  const [email, setInputId] = useState("");
+  const [password, setInputPw] = useState("");
+
+  const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -30,6 +84,8 @@ const Login = () => {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  value={email}
+                  onChange={handleInputId}
                   required
                   className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -50,6 +106,8 @@ const Login = () => {
                   id="password"
                   name="password"
                   type="password"
+                  value={password}
+                  onChange={handleInputPw}
                   autoComplete="current-password"
                   required
                   placeholder="비밀번호를 입력하세요."
@@ -65,12 +123,13 @@ const Login = () => {
               >
                 취소
               </Link>
-              <Link
-                to="/home"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              <button
+                  type="button"
+                  onClick={onClickLogin}
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 로그인
-              </Link>
+              </button>
             </div>
           </form>
         </div>
