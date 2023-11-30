8a89d6f27d827e3ce842b5ac79f8de2a3f7221e6
강병헌 <fund88@naver.com>
2023-11-30 22:52:36 +0900

    [BE] feat: sleepdiary 초기설정

--- backend/src/main/java/com/example/insomniadiary/Serverinit.java
+++ backend/src/main/java/com/example/insomniadiary/Serverinit.java
@@ -1,5 +1,7 @@
 package com.example.insomniadiary;
 
+import com.example.insomniadiary.domain.sleepdiary.SleepDiary;
+import com.example.insomniadiary.domain.sleepdiary.SleepDiaryRepository;
 import com.example.insomniadiary.domain.user.User;
 import com.example.insomniadiary.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +13,23 @@ import org.springframework.stereotype.Component;
 public class Serverinit implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final SleepDiaryRepository sleepDiaryRepository;
 
     @Override
     public void run(String[] args) throws Exception{
+        userDataInit();
+        SleepDiaryDataInit();
+    }
+
+    private void userDataInit() {
         User user = new User("admin", "admin", "정우성", 24, 72);
         userRepository.save(user);
         System.out.println("user = " + user);
+    }
+
+    private void SleepDiaryDataInit() {
+        SleepDiary sleepDiary = new SleepDiary("2023-11-30",100,19,3,17,"zolpidem",1,8,7);
+        sleepDiaryRepository.save(sleepDiary);
+        System.out.println("sleepDiary = " + sleepDiary);
     }
 }
